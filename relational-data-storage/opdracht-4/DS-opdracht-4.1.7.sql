24b241baa79870123b298105523f00691116bfb7
demi <demikraus@gmail.com>
2025-10-16 16:21:24 +0200

    DS-opdracht-4.1.7 (#4)

--- relational-data-storage/opdracht-4/DS-opdracht-4.1.7.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.7.sql
@@ -1,5 +1,5 @@
-SELECT city.name, city.id, commune.name, commune.id
-FROM mhl_cities as city
-JOIN mhl_communes AS commune ON city.commune_id = commune.id
-WHERE city.name = commune.name
-ORDER BY city.name;
+SELECT city1.name, city2.name, city1.id, city2.id, city1.commune_ID, city2.commune_ID
+FROM mhl_cities as city1 
+JOIN mhl_cities as city2 ON city1.name = city2.name
+WHERE city1.id <> city2.id
+ORDER BY city1.name
