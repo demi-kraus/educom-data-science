764f128da74ccc39e298700dbeb852b733ea872c
demi <demikraus@gmail.com>
2025-11-04 15:31:02 +0100

    case-database

---
+++ case/procedure_city_table.sql
@@ -0,0 +1,27 @@
+DROP PROCEDURE IF EXISTS city_table;
+DELIMITER &&
+
+CREATE PROCEDURE city_table()
+BEGIN
+	DECLARE rowcount INT DEFAULT 1 ;
+    DECLARE batchnr INT DEFAULT 0;
+    
+	SET SQL_SAFE_UPDATES = 0;
+    SET SESSION net_read_timeout = 600;
+	WHILE rowcount > 0 DO
+		UPDATE globaltemperaturesbycity as a
+		JOIN city as b ON a.city = b.name
+		SET a.city_id = b.id
+		WHERE a.city_id IS NULL
+		LIMIT 10000;
+        
+        SET rowcount = ROW_COUNT();
+		-- show message
+        SELECT CONCAT('Updated rows in this batch (',batchnr,':', rowcount) AS message;
+        SET batchnr = batchnr + 1;
+	END WHILE;
+    SET SQL_SAFE_UPDATES = 1;
+END &&
+    
+DELIMITER ;
+SHOW CREATE PROCEDURE city_table      
--- case/update_tables.sql
+++ case/update_tables.sql
@@ -22,3 +22,32 @@ on a.state = b.name;
 
 select * from globaltempstate;
 
+-- create globaltempmajorcity
+select * from globaltemperaturesbymajorcity;
+
+drop table globaltempmajorcity;
+
+create table globaltempmajorcity as
+select A.id, A.dt, A.averagetemperature, A.averagetemperatureuncertainty, B.id as city_id, A.latitude, A.longitude
+from globaltemperaturesbymajorcity as A
+left join city as b
+on a.city = b.name;
+
+SELECT * FROM globaltempmajorcity;
+
+-- create globaltempcity
+select * from globaltemperaturesbycity;
+
+select * from globaltemperaturesbycity where city_id IS NOT NULL;
+select COUNT(*) from globaltemperaturesbycity where city_id IS NULL;
+
+-- RUn call city_table in command line unil there city_id is set in every row. 
+-- CALL city_table;
+ALTER TABLE globaltemperaturesbycity DROP COLUMN city;
+ALTER TABLE globaltemperaturesbycity DROP COLUMN country;
+ALTER TABLE globaltemperaturesbycity RENAME globaltempcity;
+
+drop table globaltemperaturesbycity;
+drop table globaltemperaturesbycountry;
+drop table globaltemperaturesbystate;
+drop table globaltemperaturesbymajorcity;
